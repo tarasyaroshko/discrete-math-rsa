509cb2f907550781827d56bab7239e22295b9e83
tarasyaroshko <91616317+tarasyaroshko@users.noreply.github.com>
2022-05-01 20:07:26 +0300

    update rsa.py

--- rsa.py
+++ rsa.py
@@ -1,22 +1,53 @@
-from pyexpat.errors import messages
 import random
+
 class KeyGeneration:
     def __init__(self) -> None:
-        self.p_number = 11
-        self.q_number = 173
+        self.p_number, self.q_number = self.generate_p_and_q()
         self.second_part_of_the_key = self.p_number * self.q_number
         self.lambda_n = (self.p_number - 1)* (self.q_number - 1)
-        self.e_number = 17
-        self.d_number = self.inverse_mod(self.e_number, self.lambda_n)
+        self.e_number = self.calculate_second_half()
+        self.d_number = self.extended_eucledian(self.e_number, self.lambda_n)
+    def calculate_gcd(self, num1, num2):
+        while(num2):
+            num1, num2 = num2, num1%num2
+        return num1
+    def calculate_second_half(self):
+        for e in range(11, 1000, 2):
+            gcd = self.calculate_gcd(self.lambda_n, e)
+            if gcd == 1:
+                return e
+    def generate_p_and_q(self):
+        with open('prime_numbers.txt', 'r') as file:
+            lines = file.read().split("\n")
+            p = int(random.choice(lines))
+            q = int(random.choice(lines))
+            if p == q:
+                q = int(random.choice(lines))
+            return p,q
     def gcd(self, a, b):
         if a % b == 0:
             return b
         return self.gcd(b, a % b)
-    def inverse_mod(self, a, m):
-        for x in range(1, m):
-            if (((a%m) * (x%m)) % m == 1):
-                return x
-    def key_generation(self, p, q):
+    def extended_eucledian(self, x, y):
+        y_old = y
+        secret_key = 1
+        olds = 0
+        oldoldt = 0
+        oldt = 1
+        while y != 0:
+            q = x//y
+            r = x%y
+            x = y
+            y = r
+            s = secret_key - (q*olds)
+            t = oldoldt - (q*oldt)
+            secret_key = olds
+            oldoldt = oldt
+            olds = s
+            if secret_key < 0:
+                secret_key = secret_key + y_old
+        return secret_key
+    def key_generation(self):
         if self.d_number < 0:
             self.d_number += self.lambda_n
         return {'private': (self.d_number, self.second_part_of_the_key), 'public': (self.e_number, self.second_part_of_the_key)}
@@ -49,7 +80,7 @@ class Encryption:
         return r
     def encrypt(self):
         blocks = []
-        for i in range(len(message)):
+        for i in range(len(self.message)):
             number = self.fast_modular_power(ord(self.message[i]), self.public[0], self.public[1])
             blocks.append(str(number))
         return " ".join(blocks)
@@ -82,9 +113,9 @@ class Decryption:
 
 if __name__ == "__main__":
     p, q = KeyGeneration().p_number, KeyGeneration().q_number
-    keys = KeyGeneration().key_generation(p, q)
+    keys = KeyGeneration().key_generation()
     public, private = keys['public'], keys['private']
-    message = 'dfadsjkfna'
+    message = 'прфпрфд fiwughiuebiu^&$@(*@'
     encrypted = Encryption(message, public).encrypt()
     decrypted = Decryption(encrypted, private).decrypt()
     print(encrypted)
